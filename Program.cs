6278ec3a81c95ef269792765512b2788ca3100cd
Mike V <dev.mikev@gmail.com>
2018-10-18 09:05:35 -0400

    first commit - calculator

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace command_calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+
+            var x = GetNum("X");
+            var oper = GetOperator();
+            var y = GetNum("Y");
+            var calc = Calculate(x, y, oper);
+            Console.WriteLine(calc);
+        }
+
+        public static int GetNum(string variable)
+        {
+            Console.Write("Enter {0}: ", variable);
+            int num;
+            Int32.TryParse(Console.ReadLine(), out num);
+            return num;
+        }
+
+        public static String GetOperator()
+        {
+            Console.Write("Enter an operatior ' +, -, *, /': ");
+            var oper = Console.ReadLine();
+            return oper;           
+        }
+
+        public static float Calculate(float a, float b, string c)
+        {
+            float result;
+            switch (c)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+
+
+    }
+}
